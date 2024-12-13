78069815592efd255a9146411d0243fa3fad8c6b
Mazen2810 <mazenmuhammed52@gmail.com>
2024-12-13 20:35:29 +0200

    Solve Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -12,14 +12,23 @@ internal class Program
 
         #region Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
 
-        int X = 5;
-        int Y = 10;
-        Y = X;  // X = 5 , Y = 5 
-        X++;    // X = 6
-        Console.WriteLine(X);
-        Console.WriteLine(Y);
+        //int X = 5;
+        //int Y = 10;
+        //Y = X;  // X = 5 , Y = 5 
+        //X++;    // X = 6
+        //Console.WriteLine(X);
+        //Console.WriteLine(Y);
         #endregion
 
+        #region Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+        //Employee Emp01 = new Employee();
+        //Employee Emp02 = new Employee();
 
+        //Emp01.Name = "Ahmed";
+
+        //Emp02 = Emp01;
+        //Console.WriteLine(Emp02.Name);
+        // When Assigning one reference type variable to another, they become refering to the same address not The value.
+        #endregion
     }
 }
