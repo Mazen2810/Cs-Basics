5ede64f7c4dd9b90ea26ac41cdf394b0ed045622
Mazen2810 <mazenmuhammed52@gmail.com>
2024-12-13 20:16:53 +0200

    Solve Q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Assignment;   
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Write a program that allows the user to enter a number then print it.
+        //Console.WriteLine("Enter a Number: ");
+        //string Num = Console.ReadLine();
+        //Console.WriteLine(Num);
+        #endregion
+
+        
+    }
+}
